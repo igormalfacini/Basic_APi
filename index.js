d7325c078dcfef67ebd5a201ab86175c69eb3027
Igor Malfacini <84872501+igormalfacini@users.noreply.github.com>
2023-11-07 09:56:51 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+  return res.json({ message: 'Hello World!' });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running...');
+});
