c817209a60e6516183cdfd8ad968e6ab45a664de
Igor Malfacini <84872501+igormalfacini@users.noreply.github.com>
2023-11-07 12:28:25 -0300

    update: index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+  return res.send('Hello World');
 });
 
 server.listen(3000, () => {
